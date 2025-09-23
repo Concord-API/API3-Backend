7222279823835c9bb03ecd0b916fd7527f30e72e
RichardCordeiro <richard.cordeiro@dnx.tec.br>
2025-09-23 00:58:20 -0300

    feat: design pattern e estrutura do projeto

---
+++ proficio/src/main/java/com/concord/proficio/API/Controller/UsuarioController.java
@@ -0,0 +1,4 @@
+package com.concord.proficio.API.Controller;
+
+public class UsuarioController {
+}
---
+++ proficio/src/test/java/com/concord/proficio/ProficioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concord.proficio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProficioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
