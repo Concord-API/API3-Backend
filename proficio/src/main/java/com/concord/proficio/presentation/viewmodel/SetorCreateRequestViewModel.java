c3437b12fb9cb7c44df4a94b33ec359090b1aa01
RichardCordeiro <richard.leocordeiro@hotmail.com>
2025-10-22 17:59:39 -0300

    feat: adicionado Criação de setores, e busca de setores.

---
+++ proficio/src/main/java/com/concord/proficio/presentation/viewmodel/SetorCreateRequestViewModel.java
@@ -0,0 +1,15 @@
+package com.concord.proficio.presentation.viewmodel;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class SetorCreateRequestViewModel {
+	@NotBlank(message = "Nome é obrigatório")
+	@Size(max = 50)
+	private String nome;
+
+	@Size(max = 100)
+	private String descricao;
+}
---
+++ proficio/src/main/java/com/concord/proficio/presentation/viewmodel/SetorResponseViewModel.java
@@ -0,0 +1,16 @@
+package com.concord.proficio.presentation.viewmodel;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class SetorResponseViewModel {
+	private Long id;
+	private String nome;
+	private String descricao;
+	private Boolean status;
+}
+
