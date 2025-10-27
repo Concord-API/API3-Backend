dbcdcfbf7ab2e1077b862f376cee0dbcb7789296
Victor Nogueira <vgdsno@gmail.com>
2025-10-26 23:46:37 -0300

    feat: implementando método de criação de colaboradores no ColaboradorController com validações e persistência no repositório

---
+++ proficio/src/main/java/com/concord/proficio/presentation/viewmodel/ColaboradorCreateRequestViewModel.java
@@ -0,0 +1,33 @@
+package com.concord.proficio.presentation.viewmodel;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ColaboradorCreateRequestViewModel {
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private String sobrenome;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String senha;
+
+    private String genero;
+
+    @NotBlank
+    private String role;
+
+    @NotNull
+    private Long idEquipe;
+    @NotNull
+    private Long idCargo;
+
+    private Boolean status;
+}
+
+
